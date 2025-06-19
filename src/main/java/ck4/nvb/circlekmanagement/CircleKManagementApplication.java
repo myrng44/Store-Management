b002ac761d3d324b845af42df0209f566d1153cf
myrdin44 <nv.bac44@gmail.com>
2025-06-19 18:33:17 +0700

    feat : first fetch to repo

--- src/main/java/ck4/nvb/circlekmanagement/CircleKManagementApplication.java
+++ src/main/java/ck4/nvb/circlekmanagement/CircleKManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CircleKManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CircleKManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CircleKManagementApplication.class, args);
+    }
 
 }
