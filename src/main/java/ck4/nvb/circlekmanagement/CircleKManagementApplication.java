d5bca43bbdbf5a3e467c9789a51329eca8cb8acd
myrdin44 <nv.bac44@gmail.com>
2025-06-19 18:21:36 +0700

    feat : firs connect to github

---
+++ src/main/java/ck4/nvb/circlekmanagement/CircleKManagementApplication.java
@@ -0,0 +1,13 @@
+package ck4.nvb.circlekmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleKManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleKManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ck4/nvb/circlekmanagement/CircleKManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ck4.nvb.circlekmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleKManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
